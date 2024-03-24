c6774dd4b84163a612691e9bfa2d77642084954c
zerogon <safoma@naver.com>
2024-03-24 20:34:14 +0900

    join

--- note.py
+++ note.py
@@ -1,17 +1,25 @@
 '''
-[1, 4, 2, 5, 3]	[1, 2, 3]
-'''
-arr = [1, 4, 2, 5, 3]
-
-stk = []
-'''
-for i in range(len(arr)):
-    while stk and stk[-1] >= arr[i]:
-        stk.pop()
-    stk.append(arr[i])
+"rermgorpsam"	[[2, 3], [0, 7], [5, 9], [6, 10]]	"programmers"
 '''
 
-for i in range(len(arr)) :
-    while stk and stk[-1] >= arr[i] :
-        stk.pop()
-    stk.append(arr[i])
+my_string, queries = "rermgorpsam", [[2, 3], [0, 7], [5, 9], [6, 10]]
+
+list = [i for i in my_string]
+#progrmersam
+a = list[0:3].reverse()
+print(a)
+
+
+#for a, b in queries :
+ #   list[a], list[b] = list[b], list[a]
+
+#answer = "".join(list)
+#print(answer)
+'''
+[2, 3]	"remrgorpsam"
+[0, 7]	"progrmersam"
+[5, 9]	"prograsremm"
+[6, 10]	"programmers"
+'''
+
+
