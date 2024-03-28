efbe8a2667193b4f3ebcd5e400e0eaf1b6d82dd4
zerogon <safoma@naver.com>
2024-03-28 18:40:08 +0900

    org, range

--- note.py
+++ note.py
@@ -1,20 +1,14 @@
 
 '''
-q	r	code	result
-3	1	"q[j]nw[e]zg[r]pi[r]ld[y]wt"	"jerry"
-
-
-        code	q	j	n	w	e	z	g	r	p	i	r	l	d	y	w	t
-       index	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15
-q로 나눈 나머지	 0 	1	2	0	1	2	0	1	2
 '''
-# 1 , 5, 9 , 13
-q, r, code= 3,	1,	"qjnwezgrpirldywt"
+arr, idx = [0, 0, 0, 1]	,1	
+answer = 0
 
-print(code[r::q])
+for i, v in enumerate(arr) :
+    if i >= idx :
+        if v == 1 :
+            answer = i
 
 
 
-
-
-
+print(answer)
