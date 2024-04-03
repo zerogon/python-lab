1b399530e30247e86be8039ca12b39a17cc65074
zerogon <safoma@naver.com>
2024-04-03 22:03:06 +0900

    daily

--- note.py
+++ note.py
@@ -1,14 +1,10 @@
 '''
-myString	pat	return
-"AbCdEfG"	"aBc"	1
-"aaAA"	"aaaaa"	0
+myString	result
+"abstract algebra"	"AbstrAct AlgebrA"
+"PrOgRaMmErS"	"progrAmmers"
 '''
-myString , pat = "AbCdEfG", "aBc"
-print(pat.upper() in myString.upper()) # True Fales
-
-
-
-
+myString = "abstract algebra"
+answer = myString.lower().replace("a", "A")
     
 
 
