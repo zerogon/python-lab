faec10da3e3f2bb25c22469c5f9ebe034e29ef32
zerogon <safoma@naver.com>
2024-04-15 19:08:05 +0900

    daily

--- note.py
+++ note.py
@@ -1,3 +1,25 @@
 '''
-[[1,1,0],] = computers n = 3
+clothes	return
+[["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]	5
+[["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]	3
 '''
+clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+
+''
+
+'''
+headgear : [1, 2, 3]
+eyewear : [1]
+face : [1,2]
+'''
+dic={}
+answer = 1
+for a, b in clothes :
+    if b not in dic.keys() :
+        dic[b] = [a]
+    else :
+        dic[b] += [a]
+for k, v in dic.items() :
+    answer *= (len(v) + 1)
+
+print(answer)
