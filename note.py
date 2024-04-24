3091ad8c997b65081fc7451bf9f02a8b0be1692b
zerogon <safoma@naver.com>
2024-04-24 19:14:16 +0900

    daily

--- note.py
+++ note.py
@@ -1,25 +1,8 @@
 '''
-N = 5
-K = 2
-answer 3
+
+order	result
+["cafelatte", "americanoice", "hotcafelatte", "anything"]	19000
+["americanoice", "americano", "iceamericano"]	13500
 '''
-from collections import deque
 
-N, K = 5, 2
-answer = 0
-
-'''
-1 2 3 4 5
-
-1 (2) 3 4 5
-
-1 (2) 3 [4] 5
-
-[1] (2) 3 (4) 5
-
-(1) (2) 3 (4) [5]
-
-'''
-from collections import deque
-
-queue = deque(range(1))
+order = ["cafelatte", "americanoice", "hotcafelatte", "anything"]
