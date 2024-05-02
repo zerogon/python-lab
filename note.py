c724b97d3f44e9be2ec2a1d97bf03fb1903b70b9
zerogon <safoma@naver.com>
2024-05-02 18:50:58 +0900

    daily

--- note.py
+++ note.py
@@ -1,50 +1,26 @@
 '''
-s	result
-"[](){}"	3
-"}]()[{"	2
-"[)(]"	0
-"}}}"	0
 
+[1, 1, 1, 2, 3, 4, 5]	5	[6, 6]
+    [(0,3), (3,4), (6,6)]
 
-0	"[](){}"	O
-1	"](){}["	X
-
-2	"(){}[]"	O  ['[']
-
-3	"){}[]("	X
-4	"{}[]()"	O
-5	"}[](){"	X
+[2, 2, 2, 2, 2]	        6	[0, 2]
+    [(0,2), (1,3), (2,4)]
 '''
-
-s = "[)(]"
-answer = 0
-stack = []
-for i in range(len(s)):
-    # 올바른 문자열 확인
-    for j in s :
-        if j == "[" :
-            stack.append(j)
-        elif j == "(" :
-            stack.append(j)
-        elif j == "{" :
-            stack.append(j)
-        elif j == "]" :
-            if stack :
-              stack.pop()
-        elif j == ")" :
-            if stack :
-               stack.pop()
-        elif j == "}" :
-            if stack :
-              stack.pop()
-    # 맨 앞글자를 맨 뒤로 이동
-    print(i, stack)
-    if len(stack) == 0 :
-      answer +=1
-      
-
-    lst = list(s)
-    lst.append(lst.pop(0))
-    s = "".join(lst)
-    stack.clear()
-print(answer)
+sequence, k = [1, 1, 1, 2, 3, 4, 5], 5
+answer = []
+l = 0
+r = 0
+sum = 0
+'''
+sequence	             k	result
+[1, 2, 3, 4, 5]     	7	[2, 3]
+[1, 1, 1, 2, 3, 4, 5]   5
+[2, 2, 2, 2, 2]	        6	[0, 2]
+'''
+st = set()
+while l != len(sequence) :
+    sum = sequence[l]
+    r = l
+    if sum == k :
+        
+    #elif sum < k :
